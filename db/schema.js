a74369ba13dee8f6755ad5bf81902ac4581b789a
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-28 16:35:52 -0500

    Estructura para iniciar en graphqal

--- db/schema.js
+++ db/schema.js
@@ -2,21 +2,7 @@ const { gql} = require('apollo-server')
 
 //schema
 const typeDefs = gql`
-    type Curso {
-        titulo: String
-    }
-    type Tecnologia {
-        tecnologia: String
-    }
 
-    input CursoInput {
-        tecnologia: String
-    }
-
-    type Query {
-        obtenerCursos(input: CursoInput!): [Curso]
-        obtenerTecnologia: [Tecnologia]
-    }
 `;
 
 module.exports = typeDefs;
